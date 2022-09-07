c6debf4e5fd421ae9104a649a33badf2c4ab0bfa
tagijakob <tagi.jakob@gmail.com>
2022-09-08 01:30:44 +0200

    Created tutorial for OOP

---
+++ Regular Expressions tutorial.py
@@ -0,0 +1,9 @@
+import re
+# There is special documentation what each of the sings means.
+# 3 most useful ones:
+# full documentation can be found: https://docs.python.org/3.8/library/re.html
+
+re.search()
+re.findall()
+
+# 2.2 Extracting data
